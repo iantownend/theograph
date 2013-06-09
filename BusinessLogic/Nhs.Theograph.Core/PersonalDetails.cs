ca935b65402b03b048449d4a5b014d38c330cdbd
robyaw <robyaw@gmail.com>
2013-06-09 23:35:36 +0100

    Updated views to use IPersonNameMixins

--- BusinessLogic/Nhs.Theograph.Core/PersonalDetails.cs
+++ BusinessLogic/Nhs.Theograph.Core/PersonalDetails.cs
@@ -4,8 +4,9 @@
     using System.Collections.Generic;
     using System.Linq;
     using System.Text;
+    using Nhs.Theograph.Core.Interfaces;
 
-    public class PersonalDetails
+    public class PersonalDetails : IPersonName
     {
         public string Forename { get; set; }
         public string Surname { get; set; }
